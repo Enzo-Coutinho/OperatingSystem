7159f9e359441b38fb9db186b3df4db37ede0cfa
Enzo Coutinho <enzocoutinho71@gmail.com>
2025-07-12 11:48:36 -0300

    change bit field

--- main.c
+++ main.c
@@ -3,9 +3,6 @@
 
 struct bit_field {
     int data1 : 8;
-    int data2 : 8;
-    int data3 : 8;
-    int data4 : 8;
 };
 
 struct bit_field2 {
@@ -33,10 +30,7 @@ struct normal_struct ns = {
 int i = 0x12345678;
 
 struct bit_field bf = {
-    .data1 = 0x12,
-    .data2 = 0x34,
-    .data3 = 0x56,
-    .data4 = 0x78,
+    .data1 = 0x12
 };
 
 struct bit_field2 bf2 = {
