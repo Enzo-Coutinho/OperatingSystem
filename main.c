d0efbfcd05e9f443af447c885dcf030006414160
Enzo Coutinho <enzocoutinho71@gmail.com>
2025-07-12 12:17:07 -0300

    feat: multidimensional array

--- main.c
+++ main.c
@@ -1,39 +1,8 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-struct bit_field {
-    int data1 : 8;
-};
-
-struct bit_field2 {
-    int data1 : 8;
-    int data2 : 32;
-};
-
-struct normal_struct {
-    int data1;
-    int data2;
-    int data3;
-    int data4;
-};
-
-struct normal_struct ns = {
-    .data1 = 0x12345678,
-    .data2 = 0x9abcdef0,
-    .data3 = 0x12345678,
-    .data4 = 0x9abcdef0
-};
-
-int i = 0x12345678;
-
-struct bit_field bf = {
-    .data1 = 0x12
-};
-
-struct bit_field2 bf2 = {
-    .data1 = 0x12,
-    .data2 = 0x3456,
-};
+uint8_t a2[2][2] = {{0x12, 0x34}, {0x56, 0x78}};
+uint8_t a3[2][2][2] = {{{0x12, 0x34},{0x56, 0x78}},{{0x9a, 0xbc},{0xde, 0xff}}};
 
 int main()
 {
