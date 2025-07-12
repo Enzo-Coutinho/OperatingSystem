0aede3f0a3a61355727a97485bb69d1a3c34e4e9
Enzo Coutinho <enzocoutinho71@gmail.com>
2025-07-12 10:02:01 -0300

    type in C

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <inttypes.h>
+
+
+uint8_t byte = 0x12;
+uint16_t word = 0x1234;
+uint32_t dword = 0x12345678;
+uint64_t qworx = 0x123456789abcdef;
+__uint128_t dqword1 = (__uint128_t)0x123456789abcdef;
+__uint128_t dqword2 = (__uint128_t)0x123456789abcdef << 64;
 
 int main()
 {
-    printf("Hello world");
     return 0;
 }
